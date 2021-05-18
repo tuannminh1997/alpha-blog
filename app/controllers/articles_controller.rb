847b45111d65bffa8727c79284c6cf8f02b82a91
tuannminh1997 <tuannminh1997@gmail.com>
2021-05-18 15:32:00 +0700

    complete edit function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
@@ -13,6 +12,20 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
