03dfe7c3cdc40edd02c1199ab8709b46ffdba57a
tuannminh1997 <tuannminh1997@gmail.com>
2021-05-25 18:06:38 +0700

    Before save - downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+  before_save { self.email = email.downcase }
   validates :username, presence: true,
                        length: { minimum: 3, maximum: 25 },
                        uniqueness: { case_sensitive: false }
