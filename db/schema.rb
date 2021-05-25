34982587f75eab4b377766d152d6a737eeb478de
tuannminh1997 <tuannminh1997@gmail.com>
2021-05-25 09:54:28 +0700

    Add users talbe and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_155223) do
+ActiveRecord::Schema.define(version: 2021_05_25_024648) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
