4223305e1a88799377e9740b4a90d2da0227a02b
tuannminh1997 <tuannminh1997@gmail.com>
2021-05-25 21:21:27 +0700

    brcypt gem for user password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_050029) do
+ActiveRecord::Schema.define(version: 2021_05_25_121334) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_050029) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
